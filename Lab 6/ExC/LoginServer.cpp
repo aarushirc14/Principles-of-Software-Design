c3dec5779ad239cc7e807beb85da2963d7e4a389
Aarushi Roychoudhury <91914220+aarushirc14@users.noreply.github.com>
2023-04-28 09:26:17 -0600

    Update LoginServer.cpp

--- Lab 6/ExC/LoginServer.cpp
+++ Lab 6/ExC/LoginServer.cpp
@@ -1,7 +1,7 @@
 /*  ENSF 480 Fall 2022
     Lab 6 - Exercise C
     Filename: LoginServer.cpp
-    Author: Aarushi Roy Choudhury (30113987)
+    Author: Aarushi Roy Choudhury
     Date: 2022-11-15
  */
 #include <string>
