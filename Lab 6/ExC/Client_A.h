d1df6231b855008bf6c560f6f212ee631a0e5e99
Aarushi Roychoudhury <91914220+aarushirc14@users.noreply.github.com>
2023-04-28 09:25:50 -0600

    Update Client_A.h

--- Lab 6/ExC/Client_A.h
+++ Lab 6/ExC/Client_A.h
@@ -2,7 +2,7 @@
 /*  ENSF 480 Fall 2022
     Lab 6 - Exercise C
     Filename: Client_A.h
-    Author: Aarushi Roy Choudhury (30113987)
+    Author: Aarushi Roy Choudhury
     Date: 2022-11-15
  */
 #include "LoginServer.h"
